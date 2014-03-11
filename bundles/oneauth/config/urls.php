9ace34126cad7bc82d675737569f44dd6353b7a6
Julio Foulquié <jfoulquie@gmail.com>
2014-03-11 23:43:34 +0000

    Enable oneauth bundle and set up the urls on its config file.

--- bundles/oneauth/config/urls.php
+++ bundles/oneauth/config/urls.php
@@ -1,10 +1,10 @@
 <?php
 
 return array(
-	'registration' => 'auth/register',
-	'login'        => 'auth/login',
-	'callback'     => 'auth/callback',
-	
-	'registered'   => 'auth/account',
-	'logged_in'    => 'auth/account',
+	'registration' => 'register',
+	'login'        => 'login',
+	'callback'     => 'connect/callback',
+
+	'registered'   => '',
+	'logged_in'    => '',
 );
