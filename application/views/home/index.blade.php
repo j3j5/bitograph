ccd1eca274e878ff811b3db2fbcff2ef9454db7f
Julio Foulquié <jfoulquie@gmail.com>
2015-02-16 16:49:55 -0500

    Add humans.txt to the repo and the page.

--- application/views/home/index.blade.php
+++ application/views/home/index.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<link rel="author" href="humans.txt" />
 	<title>Bitcoin Price TODAY!</title>
 
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
